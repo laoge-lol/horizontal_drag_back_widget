a3a015e68cc75cdd65c212939f7d6ee7ef1abc75
laogede <2912842867@qq.com>
2025-04-21 10:47:40 +0800

    update code warning

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,6 +1,7 @@
 ///
 ///@auther Gever
 ///created at 2025/4/16/15:46
+library;
 
 class Constant{
   static const transitionDuration = 300;
--- lib/custom_drag_gesture_recognizer.dart
+++ lib/custom_drag_gesture_recognizer.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/gestures.dart';
 
 ///
